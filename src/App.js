31a1d57796936acad5d421ce80c25b68f311f0e0
Joe <joestpierre1994@gmail.com>
2023-04-19 15:20:50 -0400

    Add ImageLinkForm and Rank components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import ParticlesBg from "particles-bg";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
-import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm"
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
+      <ParticlesBg type="cobweb" bg={true} />
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+    return (
+        <div class="f3 ">
+            <p className="mb0">Joe, your current rank is...</p>
+            <p className="f1 ma0">#5</p>
+        </div>
+    )
+};
+
+export default Rank;
