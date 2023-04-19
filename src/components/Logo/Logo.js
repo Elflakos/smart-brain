54dd4fbe989c8fc744c4dcadca77ec93ed50fe58
Joe <joestpierre1994@gmail.com>
2023-04-19 14:47:33 -0400

    Add Logo component and tilt pkg

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import Brain from "./brain.png";
+import "./Logo.css";
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+            <Tilt className="br2 shadow-2 logo__background">
+                <div>
+                    <img src={Brain} alt="Brain Logo" />
+                </div>
+            </Tilt>
+        </div>
+    )
+}
+
+export default Logo;
