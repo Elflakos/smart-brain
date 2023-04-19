d8072fac9c5f4a58097e799dd4b8d4f55cda323b
Joe <joestpierre1994@gmail.com>
2023-04-19 15:03:09 -0400

    Add ImageLinkForm component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import "./Logo.css";
 
 const Logo = () => {
     return (
-        <div className="ma4 mt0">
+        <div className="ma4 mt0" style={{width: "100px", height: "100px"}}>
             <Tilt className="br2 shadow-2 logo__background">
                 <div>
                     <img src={Brain} alt="Brain Logo" />
